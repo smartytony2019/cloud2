c6446406aa1f4210f45b71403e3c8a21e644654d
smartytony <smartytony2019@gmail.com>
2020-03-08 22:39:04 +0800

    数据访问 v01

--- cloud-service-sport/src/main/java/com/xinbo/cloud/service/sport/CloudSportApplication.java
+++ cloud-service-sport/src/main/java/com/xinbo/cloud/service/sport/CloudSportApplication.java
@@ -2,8 +2,10 @@ package com.xinbo.cloud.service.sport;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.xinbo.cloud")
+@EnableSwagger2
 public class CloudSportApplication {
 
     public static void main(String[] args) {
