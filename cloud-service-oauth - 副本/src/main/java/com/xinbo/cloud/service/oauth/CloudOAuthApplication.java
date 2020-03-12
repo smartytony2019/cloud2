c529a78a01f5d5f34e524a785a25515b65f85e86
smartytony <smartytony2019@gmail.com>
2020-03-13 03:06:06 +0800

    jwt v01

---
+++ cloud-service-oauth - 副本/src/main/java/com/xinbo/cloud/service/oauth/CloudOAuthApplication.java
@@ -0,0 +1,13 @@
+package com.xinbo.cloud.service.oauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudOAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudOAuthApplication.class,args);
+    }
+
+}
--- cloud-service-oauth/src/main/java/com/xinbo/cloud/service/oauth/controller/TestController.java
+++ cloud-service-oauth/src/main/java/com/xinbo/cloud/service/oauth/controller/TestController.java
@@ -1,11 +1,57 @@
 package com.xinbo.cloud.service.oauth.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.xinbo.cloud.service.oauth.second.JwtRequest;
+import com.xinbo.cloud.service.oauth.second.JwtTokenUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.DisabledException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Objects;
 
 @RestController
 public class TestController {
 
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private JwtTokenUtil jwtTokenUtil;
+
+    @Autowired
+    private UserDetailsService jwtInMemoryUserDetailsService;
+
+    @RequestMapping(value = "/authenticate", method = RequestMethod.POST)
+    public ResponseEntity<?> createAuthenticationToken(@RequestBody JwtRequest authenticationRequest)
+            throws Exception {
+
+        authenticate(authenticationRequest.getUsername(), authenticationRequest.getPassword());
+
+        final UserDetails userDetails = jwtInMemoryUserDetailsService
+                .loadUserByUsername(authenticationRequest.getUsername());
+
+        final String token = jwtTokenUtil.generateToken(userDetails);
+
+        return ResponseEntity.ok(token);
+    }
+
+    private void authenticate(String username, String password) throws Exception {
+        Objects.requireNonNull(username);
+        Objects.requireNonNull(password);
+
+        try {
+            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
+        } catch (DisabledException e) {
+            throw new Exception("USER_DISABLED", e);
+        } catch (BadCredentialsException e) {
+            throw new Exception("INVALID_CREDENTIALS", e);
+        }
+    }
 
     @GetMapping("test")
     public String test() {
