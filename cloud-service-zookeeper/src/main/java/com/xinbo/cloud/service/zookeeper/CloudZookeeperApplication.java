5ccb7117559c1826e2dd1eb3d1c1287463e02b8c
smartytony <smartytony2019@gmail.com>
2020-03-17 00:23:55 +0800

    zookeeper

--- cloud-service-zookeeper/src/main/java/com/xinbo/cloud/service/zookeeper/CloudZookeeperApplication.java
+++ cloud-service-zookeeper/src/main/java/com/xinbo/cloud/service/zookeeper/CloudZookeeperApplication.java
@@ -1,24 +1,30 @@
 package com.xinbo.cloud.service.zookeeper;
 
 import com.xinbo.cloud.service.zookeeper.tasks.DistributedLock;
+import com.xinbo.cloud.service.zookeeper.tasks.DistributedLock2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.Async;
 
 public class CloudZookeeperApplication {
     static int n = 20;
 
+    @Async
     public static void secskill() {
-        System.out.println(--n);
+        if(n == 0) {
+            return;
+        }
+        System.out.println("  库存为-->  "+--n);
     }
 
     public static void main(String[] args) {
-
         Runnable runnable = new Runnable() {
             @Override
             public void run() {
-                DistributedLock lock = null;
+
+                DistributedLock2 lock = null;
                 try {
-                    lock = new DistributedLock("192.168.1.100:2181,192.168.1.100:2182,192.168.1.100:2183", "test");
+                    lock = new DistributedLock2("192.168.1.100:2181,192.168.1.100:2182,192.168.1.100:2183", "test");
                     lock.lock();
                     secskill();
                     System.out.println(Thread.currentThread().getName() + "正在运行");
@@ -30,7 +36,7 @@ public class CloudZookeeperApplication {
             }
         };
 
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 100; i++) {
             Thread t = new Thread(runnable);
             t.start();
         }
--- cloud-service-zookeeper/src/main/java/com/xinbo/cloud/service/zookeeper/tasks/DistributedLock.java
+++ cloud-service-zookeeper/src/main/java/com/xinbo/cloud/service/zookeeper/tasks/DistributedLock.java
@@ -5,6 +5,7 @@ import org.apache.zookeeper.data.Stat;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.CountDownLatch;
@@ -12,89 +13,58 @@ import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.Lock;
 
-/**
- * @Author: Song Kaijie
- * @Date: 2019/4/28
- */
 public class DistributedLock implements Lock, Watcher {
-    private ZooKeeper zk = null;
-    /**
-     * 根节点
-     */
-    private static final String ROOT_LOCK = "/locks";
-    /**
-     * 竞争的资源
-     */
+
+
+    private String root = "/locks";
+
+
+    private int sessionTimeout = 3000;
+
+    private CountDownLatch preLatch;
+    private CountDownLatch curLatch = new CountDownLatch(1);
+
     private String lockName;
-    /**
-     * 等待的前一个锁
-     */
-    private String WAIT_LOCK;
-    /**
-     * 当前锁
-     */
-    private String CURRENT_LOCK;
-    /**
-     * 当前锁
-     */
-    private CountDownLatch countDownLatch = new CountDownLatch(1);
 
-    private int sessionTimeout = 30000;
-    private List<Exception> exceptionList = new ArrayList<Exception>();
+    private ZooKeeper zk;
 
-    /**
-     * 配置分布式锁
-     *
-     * @param config   连接的url
-     * @param lockName 竞争资源
-     */
-    public DistributedLock(String config, String lockName) {
+    private String zNode;
+    private String waitNode;
+
+
+
+    public DistributedLock(String connectionString, String lockName)  {
         this.lockName = lockName;
         try {
-            zk = new ZooKeeper(config, sessionTimeout, this);
-            countDownLatch.await();
-            Stat stat = zk.exists(ROOT_LOCK, false);
-            if (stat == null) {
-                //如果根节点不存在，则创建根节点(为永久节点)
-                zk.create(ROOT_LOCK, new byte[0], ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
+            zk = new ZooKeeper(connectionString, sessionTimeout, this);
+            curLatch.await();
+            Stat stat = zk.exists(root, false);
+            if(stat == null) {
+                zk.create(root,new byte[0], ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
             }
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        } catch (KeeperException e) {
-            e.printStackTrace();
+        } catch (IOException ex) {
+            throw new LockException(ex);
+        } catch (KeeperException ex) {
+            throw new LockException(ex);
+        }catch (InterruptedException ex) {
+            throw new LockException(ex);
         }
     }
 
-    /**
-     * 节点监视器
-     */
-    @Override
-    public void process(WatchedEvent watchedEvent) {
-        //count数量减1
-        if (this.countDownLatch != null) {
-            this.countDownLatch.countDown();
-        }
-    }
+
+
 
     @Override
     public void lock() {
-        if (exceptionList.size() > 0) {
-            throw new LockException(exceptionList.get(0));
-        }
         try {
-            if (this.tryLock()) {
-                System.out.println(Thread.currentThread().getName() + " " + lockName + "获得了锁");
+            if(this.tryLock()){
                 return;
-            } else {
-                //等待锁
-                waitForLock(WAIT_LOCK, sessionTimeout);
             }
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        } catch (KeeperException e) {
-            e.printStackTrace();
+            this.waitForLock(waitNode, sessionTimeout);
+        }catch (KeeperException ex) {
+            throw new LockException(ex);
+        } catch (InterruptedException ex) {
+            throw new LockException(ex);
         }
     }
 
@@ -106,77 +76,75 @@ public class DistributedLock implements Lock, Watcher {
     @Override
     public boolean tryLock() {
         try {
+
             String splitStr = "_lock_";
-            if (lockName.contains(splitStr)) {
-                throw new LockException("锁名有误");
+            if(this.lockName.contains(splitStr)) {
+                throw new LockException("锁名不合法");
             }
-            CURRENT_LOCK = zk.create(ROOT_LOCK + "/" + lockName + splitStr, new byte[0], ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL_SEQUENTIAL);
-            //System.out.println(CURRENT_LOCK + "已经创建");
-            //取得所有子节点
-            List<String> subNodes = zk.getChildren(ROOT_LOCK, false);
-            //取出所有lockName的锁
-            List<String> lockObjects = new ArrayList<>();
-            for (String node : subNodes) {
-                String _node = node.split(splitStr)[0];
-                if (_node.equals(lockName)) {
-                    lockObjects.add(node);
-                }
+
+            String path = String.format("%s/%s%s", this.root, this.lockName, splitStr);
+
+            zNode = zk.create(path,new byte[0], ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL_SEQUENTIAL);
+
+            List<String> subNodes = zk.getChildren(root,false);
+
+            List<String> lockObjNodes = new ArrayList<>();
+            for(String node : subNodes) {
+                 String _node = node.split(splitStr)[0];
+                 if(_node.equals(this.lockName)) {
+                    lockObjNodes.add(_node);
+                 }
             }
-            //排序
-            Collections.sort(lockObjects);
-            //System.out.println(Thread.currentThread().getName() + "的锁是" + CURRENT_LOCK);
-            // 若当前节点为最小节点，则获取锁成功
-            if (CURRENT_LOCK.equals(ROOT_LOCK + "/" + lockObjects.get(0))) {
+
+
+            Collections.sort(lockObjNodes);
+            if(this.zNode.equals(this.root + "/"+lockObjNodes.get(0))) {
                 return true;
             }
-            //若不是最小的节点，则找到自己的前一个节点
-            String prevNode = CURRENT_LOCK.substring(CURRENT_LOCK.lastIndexOf("/") + 1);
-            WAIT_LOCK = lockObjects.get(Collections.binarySearch(lockObjects, prevNode) - 1);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        } catch (KeeperException e) {
-            e.printStackTrace();
+
+            String preZnode = zNode.substring(zNode.indexOf("/")+1);
+            waitNode = subNodes.get(Collections.binarySearch(subNodes, preZnode) + 1);
+        }catch (KeeperException ex) {
+            throw new LockException(ex);
+        } catch (InterruptedException ex) {
+            throw new LockException(ex);
         }
         return false;
     }
 
     @Override
-    public boolean tryLock(long time, TimeUnit unit) throws InterruptedException {
+    public boolean tryLock(long time, TimeUnit timeUnit){
         try {
-            if (this.tryLock()) {
+            if(this.tryLock()) {
                 return true;
             }
-            return waitForLock(WAIT_LOCK, sessionTimeout);
-        } catch (Exception e) {
-            e.printStackTrace();
+            this.waitForLock(this.waitNode, time);
+        }catch (Exception ex) {
+            throw new LockException(ex);
         }
         return false;
     }
 
-    private boolean waitForLock(String prev, long waitTime) throws KeeperException, InterruptedException {
-        Stat stat = zk.exists(ROOT_LOCK + "/" + prev, true);
-        if (stat != null) {
-            ///System.out.println(Thread.currentThread().getName() + "等待锁" + ROOT_LOCK + "/" + prev);
-            this.countDownLatch = new CountDownLatch(1);
-            // 计数等待，若等到前一个节点消失，则precess中进行countDown，停止等待，获取锁
-            this.countDownLatch.await(waitTime, TimeUnit.MILLISECONDS);
-            this.countDownLatch = null;
-            System.out.println(Thread.currentThread().getName() + " 等到了锁");
+    private boolean waitForLock(String node, long waitTime) throws InterruptedException, KeeperException {
+        Stat stat = zk.exists(this.root+"/"+this.waitNode, true);
+        if(stat != null) {
+            this.preLatch = new CountDownLatch(1);
+            this.preLatch.await(waitTime, TimeUnit.MILLISECONDS);
+            this.preLatch = null;
         }
         return true;
     }
 
+
     @Override
     public void unlock() {
+        System.out.println("unlock"+this.zNode);
         try {
-            //System.out.println("释放锁" + CURRENT_LOCK);
-            zk.delete(CURRENT_LOCK, -1);
-            CURRENT_LOCK = null;
+            zk.delete(this.zNode, 1);
+            zNode = null;
             zk.close();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        } catch (KeeperException e) {
-            e.printStackTrace();
+        } catch (Exception ex) {
+            ex.getStackTrace();
         }
     }
 
@@ -184,17 +152,20 @@ public class DistributedLock implements Lock, Watcher {
     public Condition newCondition() {
         return null;
     }
-}
 
+    @Override
+    public void process(WatchedEvent watchedEvent) {
 
-class LockException extends RuntimeException {
-    private static final long serialVersionUID = 1L;
-
-    public LockException(String e) {
-        super(e);
     }
 
-    public LockException(Exception e) {
-        super(e);
+
+    public class LockException extends RuntimeException {
+        private static final long serialVersionUID = 1L;
+        public LockException(String e){
+            super(e);
+        }
+        public LockException(Exception e){
+            super(e);
+        }
     }
 }
